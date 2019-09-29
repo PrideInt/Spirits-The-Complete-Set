0f3c974f24e8c7fc50d05e0313ddf08dc8aca1db
Daddy Pride <xixipepperd@gmail.com>
2019-09-29 06:04:08 -0400

    Create LightBeamCharge.java

---
+++ SpiritAbilities/src/me/korra/Spirits/util/LightBeamCharge.java
@@ -0,0 +1,141 @@
+package me.Pride.korra.Spirits.util;
+
+import java.util.Random;
+
+import org.bukkit.Location;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+
+import com.projectkorra.projectkorra.ability.AddonAbility;
+import com.projectkorra.projectkorra.configuration.ConfigManager;
+import com.projectkorra.projectkorra.util.ParticleEffect;
+
+import me.xnuminousx.spirits.ability.api.LightAbility;
+import me.xnuminousx.spirits.elements.SpiritElement;
+import net.md_5.bungee.api.ChatColor;
+
+public class LightBeamCharge extends LightAbility implements AddonAbility {
+	
+	private static String path = "ExtraAbilities.Prride.Spirits.Abilities.Light.LightBeam.";
+	FileConfiguration config = ConfigManager.getConfig();
+	
+	private long chargeTime;
+	private long cooldown;
+	
+	private long time;
+	public boolean charged;
+	
+	Random rand = new Random();
+
+	public LightBeamCharge(Player player) {
+		super(player);
+		
+		if (!bPlayer.canBend(this)) {
+			return;
+		}
+		chargeTime = config.getLong(path + "ChargeTime");
+		cooldown = config.getLong(path + "Cooldown");
+		
+		time = System.currentTimeMillis();
+		
+		start();
+	}
+	
+	@Override
+	public boolean isEnabled() {
+		return ConfigManager.getConfig().getBoolean("ExtraAbilities.Prride.Spirits.Abilities.Light.LightBeam.Enabled");
+	}
+
+	@Override
+	public long getCooldown() {
+		return cooldown;
+	}
+
+	@Override
+	public Location getLocation() {
+		return null;
+	}
+
+	@Override
+	public String getName() {
+		return "LightBeam";
+	}
+
+	@Override
+	public boolean isExplosiveAbility() {
+		return false;
+	}
+
+	@Override
+	public boolean isHarmlessAbility() {
+		return false;
+	}
+
+	@Override
+	public boolean isIgniteAbility() {
+		return false;
+	}
+
+	@Override
+	public boolean isSneakAbility() {
+		return true;
+	}
+
+	@Override
+	public void progress() {
+		if (!bPlayer.canBendIgnoreBinds(this)) {
+			remove();
+			return;
+		}
+		if (!player.isOnline() || player.isDead()) {
+			bPlayer.addCooldown(this);
+			remove();
+			return;
+		}
+		if (player.isSneaking()) {
+			if (System.currentTimeMillis() > time + chargeTime) {
+				charged = true;
+				ParticleEffect.SPELL_INSTANT.display(player.getLocation(), 1, 0F, 0.2F, 0F, 0.05F);
+			} else {
+				ParticleEffect.ENCHANTMENT_TABLE.display(player.getLocation().add(0, 1, 0), 1, 0.3F, 0.3F, 0.3F, 0.2F);
+			}
+		} else {
+			bPlayer.addCooldown(this);
+			remove();
+			return;
+		}
+	}
+	
+	@Override
+	public String getDescription() {
+		return SpiritElement.DARK_SPIRIT.getColor() + "By channeling all the stored energy within the bodies of "
+				+ "Light spirits, they are able to release it in the form of a deadly beam!";
+	}
+	
+	@Override
+	public String getInstructions() {
+		return ChatColor.GOLD + "To use, hold sneak until white spell particles appear and left click.";
+	}
+
+	@Override
+	public String getAuthor() {
+		return SpiritElement.DARK_SPIRIT.getColor() + "" + ChatColor.UNDERLINE + 
+				"Prride";
+	}
+
+	@Override
+	public String getVersion() {
+		return SpiritElement.DARK_SPIRIT.getColor() + "" + ChatColor.UNDERLINE + 
+				"VERSION 1";
+	}
+
+	@Override
+	public void load() {
+	}
+
+	@Override
+	public void stop() {
+		super.remove();
+	}
+
+}
