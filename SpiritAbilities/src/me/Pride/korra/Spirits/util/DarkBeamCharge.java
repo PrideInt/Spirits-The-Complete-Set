ea795e5c434d3ae49e2519e2059db1ee563e937c
Daddy Pride <xixipepperd@gmail.com>
2020-04-06 02:08:07 -0400

    Update DarkBeamCharge.java

--- SpiritAbilities/src/me/Pride/korra/Spirits/util/DarkBeamCharge.java
+++ SpiritAbilities/src/me/Pride/korra/Spirits/util/DarkBeamCharge.java
@@ -123,7 +123,7 @@ public class DarkBeamCharge extends DarkAbility implements AddonAbility {
 	@Override
 	public String getVersion() {
 		return SpiritElement.LIGHT_SPIRIT.getColor() + "" + ChatColor.UNDERLINE + 
-				"VERSION 2";
+				"VERSION 3";
 	}
 
 	@Override
